e34f121a750bd28330fe1ef0db1627672e0af661
Yuvasree D <yuvidakshan2000@gmail.com>
2021-05-13 18:53:00 +0530

    Final commit

--- app.js
+++ app.js
@@ -153,6 +153,12 @@ app.post("/addSpice",function(req,res){
         res.redirect("/addSpice");
     });
 })
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
